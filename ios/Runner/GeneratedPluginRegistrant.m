62e6717eee1be37278c44d6738bd131490a2c56d
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2020-08-13 08:58:31 +0530

    feat: 🚀 created flutter demo project

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -4,16 +4,9 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<flutter_blue/FlutterBluePlugin.h>)
-#import <flutter_blue/FlutterBluePlugin.h>
-#else
-@import flutter_blue;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FlutterBluePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBluePlugin"]];
 }
 
 @end
